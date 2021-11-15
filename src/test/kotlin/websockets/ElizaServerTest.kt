f439577ca89f6fc25d55d0cca5a74e72edcfb543
topopelon <jlaguna98@gmail.com>
2021-11-15 22:55:22 +0100

    test completed

--- src/test/kotlin/websockets/ElizaServerTest.kt
+++ src/test/kotlin/websockets/ElizaServerTest.kt
@@ -1,8 +1,8 @@
 package websockets
 
 import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.BeforeEach
-import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment.*
@@ -27,17 +27,18 @@ class ElizaServerTest {
 
     @Test
     fun onOpen() {
+        // Like wait(3), expecting 3 signals from other threads
         val latch = CountDownLatch(3)
         val list = mutableListOf<String>()
-
+        // Start a websocket client
         val client = ElizaOnOpenMessageHandler(list, latch)
+        // Start a websocket connection with the server
         container.connectToServer(client, URI("ws://localhost:$port/eliza"))
         latch.await()
         assertEquals(3, list.size)
         assertEquals("The doctor is in.", list[0])
     }
 
-    @Disabled
     @Test
     fun onChat() {
         val latch = CountDownLatch(4)
@@ -46,14 +47,18 @@ class ElizaServerTest {
         val client = ElizaOnOpenMessageHandlerToComplete(list, latch)
         container.connectToServer(client, URI("ws://localhost:$port/eliza"))
         latch.await()
-        // assertEquals(XXX, list.size) COMPLETE ME
-        // assertEquals(XXX, list[XXX]) COMPLETE ME
+        // [list.size] will be 4 or 5 depending of pc speed, showing also `---`
+        assertTrue(list.size in (4..5))
+        assertEquals("We were discussing you, not me.", list[3])
     }
 
 }
 
 @ClientEndpoint
-class ElizaOnOpenMessageHandler(private val list: MutableList<String>, private val latch: CountDownLatch)  {
+class ElizaOnOpenMessageHandler(
+    private val list: MutableList<String>,
+    private val latch: CountDownLatch
+) {
     @OnMessage
     fun onMessage(message: String) {
         list.add(message)
@@ -62,14 +67,21 @@ class ElizaOnOpenMessageHandler(private val list: MutableList<String>, private v
 }
 
 @ClientEndpoint
-class ElizaOnOpenMessageHandlerToComplete(private val list: MutableList<String>, private val latch: CountDownLatch)  {
+class ElizaOnOpenMessageHandlerToComplete(
+    private val list: MutableList<String>,
+    private val latch: CountDownLatch
+) {
 
     @OnMessage
-    fun onMessage(message: String, session: Session)  {
+    fun onMessage(message: String, session: Session) {
+        // Store received message from server
         list.add(message)
+        // Like signal(), signals [latch] wait once
         latch.countDown()
-        // if (COMPLETE ME) {
-        //    COMPLETE ME
-        // }
+        // When last signal()
+        if (latch.count == 1L) {
+            // Send the server a message
+            session.basicRemote.sendText("you")
+        }
     }
 }
